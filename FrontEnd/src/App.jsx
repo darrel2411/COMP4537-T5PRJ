67d24b3ccd997354f095b1b01105364ca5647ed6
Manases Villalobos <manasesvillalobos80@gmail.com>
2025-10-31 09:49:15 -0700

    Use user context in main page and protected routes component in routes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,21 +1,47 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from "./pages/Login";
+import Login from "./pages/LoginForm/Login";
 import Main from "./pages/Main";
-import SignUp from "./pages/Signup";
+import NotFoundPage from './pages/NotFound/NotFoundPage';
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Register from "./pages/RegisterForm/Register";
+import { UserProvider, useUser } from "./context/UserContext";
 
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/main" />} />
-        <Route path="/login" element={< Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/main" element={<Main />} />
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </UserProvider>
   )
+}
+
+function AppRoutes() {
+  const { user, loading } = useUser();
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/main" />} />
+
+      {/* Block user with valid session */}
+      <Route path="/login" element={user ? <Navigate to="/main" replace /> : < Login />} />
+      <Route path="/register" element={user ? <Navigate to="/main" replace /> : <Register />} />
+
+      {/* Protected routes (Valid session needed) */}
+      <Route path="/main" element={
+        <ProtectedRoute>
+          <Main />
+        </ProtectedRoute>
+      } />
+
+      {/* 404 Not found routes */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
 export default App
--- FrontEnd/src/pages/Main.jsx
+++ FrontEnd/src/pages/Main.jsx
@@ -1,29 +1,28 @@
 import { useState } from "react";
+import { useUser } from "../context/UserContext";
 
 function Main() {
-  const [msg, setMsg] = useState("");
+  const { user, setUser } = useUser();
 
   const API_BASE = import.meta.env.VITE_API_BASE;
 
-  const fetchQuote = async () => {
-    const res = await fetch(`${API_BASE}`);
-    const data = await res.json();
-    setMsg(data.msg);
-  }
+  const handleLogout = async () => {
+    await fetch(`${API_BASE}/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
+    setUser(null); // clear context
+  };
 
-  console.log(msg);
+
 
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
 
-        <h1 className="text-3xl font-bold mb-6 text-gray-800">Hello, Click button to get message from server</h1>
+        <h1 className="text-3xl font-bold mb-6 text-gray-800">Hello, { user.name }! Click bellow to logout</h1>
         
-        <button onClick={fetchQuote}>Get Message</button>
-
-        <h1 className="underline">
-          {msg}
-        </h1>
+        <button onClick={handleLogout}>Logout</button>
 
       </div>
     </>
