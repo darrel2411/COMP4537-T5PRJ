9b83338a8108341c1a556f754a974f28d7eb7346
Darrel Tapilaha <tapsdarrel@gmail.com>
2025-11-21 23:38:09 -0800

    Modified getUser query to also return api_consumption and score from user table

--- BackEnd/database/users.js
+++ BackEnd/database/users.js
@@ -30,7 +30,7 @@ async function createUser(userData) {
 
 async function getUser(email) {
     const getUserSQL = `
-        SELECT user_id, email, password, name, user_type_id
+        SELECT user_id, email, password, name, user_type_id, api_consumption, score
         FROM user
         WHERE email = :email;
     `;
